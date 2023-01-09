d6a8cfb023977edf25905781a27d754b12571284
it_bear <voxel.mco@gmail.com>
2023-01-09 18:10:37 +0400

    Fix tabs

--- Task.hpp
+++ Task.hpp
@@ -12,28 +12,27 @@ namespace Tasks {
  * Derived classes must implement the Run() function to define the behavior of the task.
  */
 
-
 class Task
 {
 public:
-	/**
-	 * @brief Alias for a shared pointer to a Task object.
-	 */
-	using SP = std::shared_ptr<Task>;
+    /**
+     * @brief Alias for a shared pointer to a Task object.
+     */
+    using SP = std::shared_ptr<Task>;
 
 protected:
-					Task	(void) noexcept = default;
-					Task	(const Task&) noexcept = delete;
-					Task	(Task&&) noexcept = delete;
+                    Task    (void) noexcept = default;
+                    Task    (const Task&) noexcept = delete;
+                    Task    (Task&&) noexcept = delete;
 
 public:
-	virtual			~Task	(void) noexcept = default;
+    virtual         ~Task   (void) noexcept = default;
 
 public:
-	/**
-	 * @brief This function defines the behavior of the task.
-	 */
-	virtual void	Run		(void) = 0;
+    /**
+     * @brief This function defines the behavior of the task.
+     */
+    virtual void    Run     (void) = 0;
 };
 
 }//namespace Tasks
